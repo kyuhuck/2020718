53b9016f17deb12a3a33d4872e8828ff95fb42e8
kyuhuckman <kyuhuckmc@nave.com>
2021-07-19 23:58:27 +0900

    rrr

--- src/views/home.vue
+++ src/views/home.vue
@@ -10,18 +10,43 @@
                     <b-img thumbnail fluid  :src="user.pgm_img_url" alt="Image 1"></b-img>
                 </b-col>
 
-
-
             </b-row>
         </b-container>
+<!--        <ul>-->
+<!--        <li v-for="(user,i) in users" :key="i" ><img :src="user.pgm_img_url" width="100" height="100"></li>-->
+
+
+<!--        </ul>-->
+
+        <slick ref="slick" :options="slickOptions">
+            <div  v-for="(user,i) in users" :key="i" >  <img :src="user.pgm_img_url" ></div>
+
+
+
+        </slick>
+
     </div>
+
+
 
 </template>
 
 
 <script>
     import axios from "axios";
+    import Slick from 'vue-slick';
+    //import VueSlickCarousel from 'vue-slick-carousel'
+    import 'vue-slick-carousel/dist/vue-slick-carousel.css'
+    // optional style for arrows & dots
+    import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
+    import 'slick-carousel/slick/slick.css';
     export default {
+
+        name: 'MyComponent',
+        components: {Slick },
+
+
+
         mounted() {
             var vm  = this;
             axios
@@ -42,14 +67,39 @@
                 });
 
         },
-
+        beforeUpdate() {
+            if (this.$refs.slick) {
+                this.$refs.slick.destroy();
+            }
+        },
+        updated() {
+            this.$nextTick(function () {
+                if (this.$refs.slick) {
+                    this.$refs.slick.create(this.slickOptions);
+                }
+            });
+        },
         data() {
             return{
                 email : null,
                 users : [],
                 name  : "kyuhuck",
-                password:null
+                password:null,
+                slickOptions: {
+                    //options can be used from the plugin documentation
+                    slidesToShow: 1,
+                    infinite: true,
+                    accessibility: true,
+                    adaptiveHeight: false,
+                    arrows: true,
+                    dots: true,
+                    draggable: true,
+                    edgeFriction: 0.30,
+                    swipe: true
+
+                }
             }
+
         },
         methods: {
             test() {
