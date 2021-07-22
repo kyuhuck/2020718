56907003c8ab1b4669e139040793c747d81b4bd3
kyuhuckman <kyuhuckmc@nave.com>
2021-07-22 18:05:49 +0900

    rrr

--- src/views/tv.vue
+++ src/views/tv.vue
@@ -3,7 +3,8 @@
         <h1>여기는tv세션입니다</h1>
         <div v-for="(user,i) in users" :key="i" >{{user.title}}</div>
 
-        <button v-on:click="test">최상단상단 타이틀 불러오기</button>
+        <button v-on:click="test">최상단상단 타이틀 불러오기</button><br>
+        조회수:{{cnt}}<button v-on:click="test2">좋아요</button>
     </div>
 
 </template>
@@ -14,11 +15,16 @@
 
         data() {
             return{
-                email : null,
+                cnt : 0,
                 users : [],
                 name  : "kyuhuck",
                 password:null
             }
+        },
+        watch :{
+            cnt(){
+            alert("증가이벤트가 발생하였습니다.");
+            },
         },
         methods: {
             test() {
@@ -40,7 +46,12 @@
                         // always executed
                     });
 
-            }
+            },
+            test2() {
+                this.cnt++;
+
+            },
+
         }
 
 
